d593cf2da6ad68a73416fc9f0314664b11ec9954
Mateus Pessoal <mateus.freitas7589@gmail.com>
2025-04-18 22:55:06 -0300

    Initial struct

---
+++ internal/usecase/dto/foo_output.go
@@ -0,0 +1,10 @@
+package dto
+
+import "time"
+
+type FooOutput struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Type      string    `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
+}
